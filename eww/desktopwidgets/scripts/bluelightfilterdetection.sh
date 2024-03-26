2e4ce1e22d619cfe4586b9378fb4480347314737
sandsq <116196797+sandsq@users.noreply.github.com>
2024-03-25 22:25:06 -0400

    Rework color scheme

--- eww/desktopwidgets/scripts/bluelightfilterdetection.sh
+++ eww/desktopwidgets/scripts/bluelightfilterdetection.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-filteroncolor="#7d2b00"
+filteroncolor="#CF5A2A"
 filteroffcolor="#003e7d"
 iconcolor=$filteroffcolor
 othericoncolor=$filteroncolor
@@ -7,7 +7,7 @@ shadestate=`hyprshade current`
 togglecommand="auto"
 
 # set icon color based on if hyprshade is auto or off, not whether the bluelight filter is on or off. Vibrance is the shader during the day if hyprshade is auto, while a blank shader is present if hyprshade is off.
-if [[ $shadestate == "blue-light-filter" || $shadestate == "vibrance" ]]; then
+if [[ $shadestate == "blue-light-filter-3000" || $shadestate == "blue-light-filter-3500" || $shadestate == "blue-light-filter-4000" || $shadestate == "blue-light-filter-4500" || $shadestate == "blue-light-filter-5000" || $shadestate == "blue-light-filter-5500" || $shadestate == "vibrance" ]]; then
 	iconcolor=$filteroncolor
 	othericoncolor=$filteroffcolor
 	togglecommand="off"
