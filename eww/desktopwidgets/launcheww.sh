f7bcddc0d1af521d366162e3352d4454369c64cf
sandsq <116196797+sandsq@users.noreply.github.com>
2024-04-30 13:49:49 -0400

    eww update fixes and weather icon spacing

--- eww/desktopwidgets/launcheww.sh
+++ eww/desktopwidgets/launcheww.sh
@@ -4,13 +4,21 @@ configfile="/home/sand/Dropbox/linux/bars/eww/desktopwidgets/"
 # eww daemon --config ./
 # sleep 1
 eww daemon --config $configfile
-eww open barwindow --config $configfile
+sleep 0.1
 eww open clockwindow --config $configfile
+sleep 0.1
 eww open calendarwindow --config $configfile
+sleep 0.1
 eww open weatherwindow --config $configfile
+sleep 0.1
 eww open diskwindow --config $configfile
+sleep 0.1
 eww open suntimewindow --config $configfile
+sleep 0.1
 eww open infowindow --config $configfile
-sleep 1
+sleep 0.1
 eww open appswindow --config $configfile
+sleep 0.1
 eww open appstabletwindow --config $configfile
+sleep 0.1
+eww open barwindow --config $configfile
